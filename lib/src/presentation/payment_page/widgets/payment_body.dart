1dca1ab1b887733e1a0bf934fdba714f0e61f3e6
a-givertzman <a.givertzman@icloud.com>
2024-10-08 19:52:08 +0300

    Payment | Page & bode created

--- lib/src/presentation/payment_page/widgets/payment_body.dart
+++ lib/src/presentation/payment_page/widgets/payment_body.dart
@@ -1,15 +1,11 @@
 import 'package:ext_rw/ext_rw.dart';
-import 'package:flowers_admin/src/core/translate/translate.dart';
 import 'package:flowers_admin/src/infrostructure/app_user/app_user.dart';
 import 'package:flowers_admin/src/infrostructure/app_user/app_user_role.dart';
 import 'package:flowers_admin/src/infrostructure/schamas/entry_customer.dart';
 import 'package:flowers_admin/src/infrostructure/schamas/entry_customer_order.dart';
-import 'package:flowers_admin/src/infrostructure/schamas/entry_transaction.dart';
-import 'package:flowers_admin/src/infrostructure/transaction/transaction_sqls.dart';
+import 'package:flowers_admin/src/infrostructure/schamas/entry_purchase_item.dart';
 import 'package:flowers_admin/src/presentation/core/table_widget/table_widget.dart';
 import 'package:flowers_admin/src/presentation/core/table_widget/table_widget_add_action.dart';
-import 'package:flowers_admin/src/presentation/transaction_page/widgets/add_transaction_form.dart';
-import 'package:flowers_admin/src/presentation/transaction_page/widgets/edit_transaction_form.dart';
 import 'package:flutter/material.dart';
 import 'package:hmi_core/hmi_core_log.dart';
 import 'package:hmi_core/hmi_core_result_new.dart';
@@ -92,37 +88,26 @@ class _PaymentBodyState extends State<PaymentBody> {
           ),
           fields: [
             const Field(hidden: false, editable: false, key: 'id'),
-            Field(hidden: false, editable: editableAuthor, title: '${InRu('Author')}', key: 'author_id', relation: const Relation(id: 'author_id', field: 'name')),
-            Field(hidden: false, editable: editableValue, title: '${InRu('Value')}', key: 'value'),
-            Field(hidden: false, editable: editableDetails, title: '${InRu('TransactionDetails')}', key: 'details'),
-            const Field(hidden: true, editable: true, key: 'order_id'),
-            Field(hidden: false, editable: false, title: '${InRu('Customer')}', key: 'customer_id', relation: const Relation(id: 'customer_id', field: 'name')),
-            Field(hidden: false, editable: false, title: '${InRu('CustomerAccountBefore')}', key: 'customer_account'),
-            Field(hidden: false, editable: true, title: '${InRu('Description')}', key: 'description'),
-            Field(hidden: false, editable: false, title: '${InRu('Created')}', key: 'created'),
-            const Field(hidden: true, editable: false, key: 'updated'),
-            const Field(hidden: true, editable: false, key: 'deleted'),
+            const Field(hidden: false, editable: false, key: 'customer_id', relation: Relation(id: 'customer_id', field: 'name')),
+            // const Field(hidden: false, editable: false, key: 'customer'),
+            const Field(hidden: false, editable: true, key: 'purchase_item_id'),
+            const Field(hidden: false, editable: true, title: 'Purchase id', key: 'purchase_item_id', relation: Relation(id: 'purchase_item_id', field: 'purchase')),
+            const Field(hidden: false, editable: true, title: 'Product id', key: 'purchase_item_id', relation: Relation(id: 'purchase_item_id', field: 'product')),
+            // const Field(hidden: false, editable: true, key: 'purchase'),
+            // const Field(hidden: false, editable: true, key: 'product'),
+            const Field(hidden: false, editable: true, key: 'count'),
+            const Field(hidden: false, editable: true, key: 'paid'),
+            const Field(hidden: false, editable: true, key: 'distributed'),
+            const Field(hidden: false, editable: true, key: 'to_refound'),
+            const Field(hidden: false, editable: true, key: 'refounded'),
+            const Field(hidden: false, editable: true, key: 'description'),
+            const Field(hidden: true, editable: true, key: 'created'),
+            const Field(hidden: true, editable: true, key: 'updated'),
+            const Field(hidden: true, editable: true, key: 'deleted'),
           ],
         ),
         relations: {
           'customer_id': TableSchema<EntryCustomer, void>(
-            read: SqlRead<EntryCustomer, void>(
-              address: _apiAddress, 
-              authToken: _authToken, 
-              database: _database, 
-              sqlBuilder: (sql, params) {
-                return Sql(sql: 'select id, name, account from customer order by id;');
-              },
-              entryBuilder: (row) => EntryCustomer.from(row.cast()),
-              debug: true,
-            ),
-            fields: [
-              const Field(key: 'id'),
-              const Field(key: 'name'),
-              const Field(key: 'account'),
-            ],
-          ),
-          'author_id': TableSchema<EntryCustomer, void>(
             read: SqlRead<EntryCustomer, void>(
               address: _apiAddress, 
               authToken: _authToken, 
@@ -136,6 +121,25 @@ class _PaymentBodyState extends State<PaymentBody> {
             fields: [
               const Field(key: 'id'),
               const Field(key: 'name'),
+            ],
+          ),
+          'purchase_item_id': TableSchema<EntryPurchaseItem, void>(
+            read: SqlRead<EntryPurchaseItem, void>(
+              address: _apiAddress, 
+              authToken: _authToken, 
+              database: _database, 
+              sqlBuilder: (sql, params) {
+                return Sql(sql: 'select id, purchase_id, purchase, product_id, product from purchase_item_view order by id;');
+              },
+              entryBuilder: (row) => EntryPurchaseItem.from(row),
+              debug: true,
+            ),
+            fields: [
+              const Field(key: 'id'),
+              const Field(key: 'purchase_id'),
+              const Field(key: 'purchase'),
+              const Field(key: 'product_id'),
+              const Field(key: 'product'),
             ],
           ),
         },                  
