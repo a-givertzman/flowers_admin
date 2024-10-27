1ecb43df8ecc1c0fc6e82c61d7680dd6399f0284
Anton Lobanov <a.givertzman@icloud.com>
2024-10-27 16:56:45 +0300

    Payment | initial refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,24 @@ import 'dart:async';
 import 'package:flowers_admin/src/app_widget.dart';
 import 'package:flowers_admin/src/core/log/log.dart';
 import 'package:flowers_admin/src/core/log/log_level.dart';
+// ignore: implementation_imports
+import 'package:hmi_core/src/core/json/json_map.dart';
+// ignore: implementation_imports
+import 'package:hmi_core/src/core/text_file.dart';
+import 'package:flowers_admin/src/core/settings/app_settings.dart';
 import 'package:flutter/material.dart';
-
+///
+///
 void main() async {
   const apiAuthToken = '';
   Log.initialize(level: LogLevel.all);
+  await AppSettings.initialize(
+    jsonMap: JsonMap.fromTextFile(
+      const TextFile.asset(
+        'assets/settings/app-settings.json',
+      ),
+    ),
+  );
   runZonedGuarded(
     () async {
       // final appThemeSwitch = AppThemeSwitch();
@@ -22,4 +35,5 @@ void main() async {
       final trace = stackTrace.toString().isEmpty ? StackTrace.current : stackTrace.toString();
       const Log('main').error('message: $error\nstackTrace: $trace'); 
     },
-  );}
+  );
+}
