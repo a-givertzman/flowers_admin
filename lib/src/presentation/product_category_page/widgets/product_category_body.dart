f30da584c8ab42c650e24183119e26a9c90540f5
Anton Lobanov <a.givertzman@icloud.com>
2025-04-14 19:00:14 +0300

    ProductCategoryPage | fixes

--- lib/src/presentation/product_category_page/widgets/product_category_body.dart
+++ lib/src/presentation/product_category_page/widgets/product_category_body.dart
@@ -4,6 +4,8 @@ import 'package:flowers_admin/src/infrostructure/app_user/app_user.dart';
 import 'package:flowers_admin/src/infrostructure/app_user/app_user_role.dart';
 import 'package:flowers_admin/src/infrostructure/product/entry_product_category.dart';
 import 'package:flowers_admin/src/presentation/core/table_widget/table_widget.dart';
+import 'package:flowers_admin/src/presentation/core/table_widget/table_widget_add_action.dart';
+import 'package:flowers_admin/src/presentation/product_category_page/widgets/edit_product_category_form.dart';
 import 'package:flutter/material.dart';
 import 'package:hmi_core/hmi_core_log.dart';
 import 'package:hmi_core/hmi_core_result.dart';
@@ -106,8 +108,77 @@ class _ProductCategoryBodyState extends State<ProductCategoryBody> {
   @override
   Widget build(BuildContext context) {
     _log.debug('.build | ');
-    return TableWidget(
+    return TableWidget<EntryProductCategory, void>(
       showDeleted: [AppUserRole.admin].contains(widget.user.role) ? false : null,
+      fetchAction: TableWidgetAction(
+        onPressed: (schema) {
+          return Future.value(Ok(EntryProductCategory.empty()));
+        }, 
+        icon: const Icon(Icons.add),
+      ),
+      addAction: TableWidgetAction(
+        onPressed: (schema) {
+          return showDialog<Result<EntryProductCategory, void>?>(
+            context: context, 
+            builder: (_) => EditProductCategoryForm(
+              user: widget.user,
+              fields: schema.fields,
+              relations: schema.relations,
+            ),
+          ).then((result) {
+            // _log.debug('.build | new entry: $result');
+            return switch (result) {
+              Ok(:final value) => Ok(value),
+              Err(:final error) => Err(error),
+              _ => const Err(null),
+            };
+          });
+        }, 
+        icon: const Icon(Icons.add),
+      ),
+      // editAction: TableWidgetAction(
+      //   onPressed: (schema) {
+      //     final toBeUpdated = schema.entries.values.where((e) {
+      //       return e.isSelected;
+      //     }).toList();
+      //     if (toBeUpdated.isNotEmpty) {
+      //       return showDialog<Result<EntryCustomer, void>?>(
+      //         context: context, 
+      //         builder: (_) => EditCustomerForm(user: _user, entry: toBeUpdated.lastOrNull),
+      //       ).then((result) {
+      //         _log.debug('.build | edited entry: $result');
+      //         return switch (result) {
+      //           Ok(:final value) => Ok(value),
+      //           Err(:final error) => Err(error),
+      //           _ => const Err(null),
+      //         };
+      //       });
+      //     }
+      //     return Future.value(Err(null));
+      //   }, 
+      //   icon: const Icon(Icons.add),
+      // ),      
+      // delAction: TableWidgetAction(
+      //   onPressed: (schema) {
+      //     final toBeDeleted = schema.entries.values.firstWhereOrNull((e) {
+      //         return e.isSelected;
+      //     });
+      //     if (toBeDeleted != null) {
+      //       return showConfirmDialog(
+      //         context, 
+      //         const Text('Delete Customer'), 
+      //         Text('Are you sure want to delete following:\n${toBeDeleted.value('name').str}'),
+      //       ).then((value) {
+      //         return switch (value) {
+      //           Ok() => Ok(toBeDeleted),
+      //           Err(:final error) => Err(error),
+      //         };
+      //       });
+      //     }
+      //     return Future.value(const Err(null));
+      //   },
+      //   icon: const Icon(Icons.add),
+      // ),
       schema: _schema,
 
     );
